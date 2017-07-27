ffb50d77950beb5b7f9dcdd043baba378721230d
Ivica <ivicajangel@gmail.com>
2017-07-27 13:59:24 +0100

    Update the API version in the Scrapper.php file
    The API version in the Scrapper file was 3.8.2 which is causing search error and bad result ( it's returning HTML instead of JSON ) - changing the API version is fixing this issue.

--- src/Scraper.php
+++ src/Scraper.php
@@ -12,7 +12,7 @@ use projectivemotion\PhpScraperTools\CacheScraper;
 
 class Scraper extends CacheScraper
 {
-    const default_api_version = '3.8.2';
+    const default_api_version = '6.6.2';
     protected $protocol =   'https';
     protected $domain   =   'be.wizzair.com';
 
